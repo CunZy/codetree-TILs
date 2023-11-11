823e594a0f260a2f6742f374e33f4b8c41c9df6a
CunZy <140526291+CunZy@users.noreply.github.com>
2023-11-11 15:43:33 +0900

    Title: 총합 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231111/총합 구하기/find-the-total.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b;
+    cin >> a >> b;
+    int s = 0;
+    for(int i = a; i <= b; ++i) {
+        if(i % 6 == 0 && i % 8 != 0) {
+            s += i;
+        }
+    }
+
+    cout << s;
+    return 0;
+}
