6e0a5f7e3274f6c946ab321a909ec4a9ffb2cede
CunZy <140526291+CunZy@users.noreply.github.com>
2023-11-11 15:17:50 +0900

    Title: 청소당번 정하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231111/청소당번 정하기/cleaning-numbering.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+    int a = 0, b = 0, c = 0;
+    for(int i = 1; i <= n; ++i) {
+        if(i % 12 == 0) {
+            c++;
+        }
+        else if(i % 3 == 0) {
+            ++b;
+        }
+        else if(i % 2 == 0) {
+            ++a;
+        }
+    }
+
+    cout << a << " " << b << " " << c;
+    return 0;
+}
