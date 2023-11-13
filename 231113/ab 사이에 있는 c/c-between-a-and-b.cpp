9b0e129d2c43a5e3478bb144a00aacd051f4a3cc
CunZy <140526291+CunZy@users.noreply.github.com>
2023-11-13 09:34:31 +0900

    Title: ab 사이에 있는 c, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231113/ab 사이에 있는 c/c-between-a-and-b.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    bool t = false;
+    for(int i = a; i <= b; ++i) {
+        if(i % c == 0) {
+            t = true;
+        }
+    }
+
+    if(t == true) {
+        cout << "YES";
+    }
+    else {
+        cout << "NO";
+    }
+    return 0;
+}
