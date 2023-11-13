f898efbefee97067c88ddb1c82a7d818ce05bb4b
CunZy <140526291+CunZy@users.noreply.github.com>
2023-11-13 09:42:54 +0900

    Title: 소수 판별기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231113/소수 판별기/prime-number-discriminator.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+    bool t = true;
+
+    for(int i = 2; i < n; ++i) {
+        if(n % i == 0) {
+            t = false;
+        }
+    }
+
+    if(t == false) {
+        cout << "C";
+    }
+    else {
+        cout << "P";
+    }
+    return 0;
+}
