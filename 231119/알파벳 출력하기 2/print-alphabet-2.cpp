da98c58af34b5ae600a347da1b6f926449d6c62a
CunZy <140526291+CunZy@users.noreply.github.com>
2023-11-19 12:59:26 +0900

    Title: 알파벳 출력하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231119/알파벳 출력하기 2/print-alphabet-2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+    int x = 65;
+
+    for(int i = 0; i < n; ++i) {
+        for(int j = 0; j < i; ++j) {
+            cout << "  ";
+        }
+        for(int j = 0; j < n - i; ++j) {
+            cout << (char)x << " ";
+            ++x;
+            if(x >= 91) {
+                x = 65;
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
