1990eee3180875ce4adb723587df786bcdf14b81
CunZy <140526291+CunZy@users.noreply.github.com>
2023-11-22 14:24:58 +0900

    Title: 전항의 두 배, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231122/전항의 두 배/twice-the-previous.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a[10];
+    cin >> a[0] >> a[1];
+
+    for(int i = 2; i < 10; ++i) {
+        a[i] = a[i - 1] + 2 * a[i - 2];
+    }
+
+    for(int i = 0; i < 10; ++i) {
+        cout << a[i] << " ";
+    }
+    return 0;
+}
