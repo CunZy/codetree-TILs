ec4d19e3269bcba26822fe25945b753000b531d5
CunZy <140526291+CunZy@users.noreply.github.com>
2023-11-24 13:00:38 +0900

    Title: 배열 놀이, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231124/배열 놀이/play-with-array.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int arr[100] = {};
+
+    int n, q;
+    cin >> n >> q;
+
+    for(int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+
+    for(int i = 0; i < q; ++i) {
+        int t;
+        cin >> t;
+        int a, b;
+        if(t == 1) {
+            cin >> a;
+            cout << arr[a - 1] << endl;
+        }
+        else if(t == 2) {
+            cin >> a;
+            int r = 0;
+            for(int j = 0; j < n; ++j) {
+                if(arr[j] == a) {
+                    r = j;
+                    break;
+                }
+            }
+            cout << r + 1 << endl;
+        }
+        else if(t == 3) {
+            cin >> a >> b;
+            for(int j = a - 1; j < b; ++j) {
+                cout << arr[j] << " ";
+            }
+            cout << endl;
+        }
+    }
+    return 0;
+}
