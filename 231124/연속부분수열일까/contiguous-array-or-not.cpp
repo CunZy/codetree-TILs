97bd2466f7977af38c609ea8030b041c2ee0e7ee
CunZy <140526291+CunZy@users.noreply.github.com>
2023-11-24 13:19:42 +0900

    Title: 연속부분수열일까, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231124/연속부분수열일까/contiguous-array-or-not.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n1, n2;
+    cin >> n1 >> n2;
+    int a[100], b[100];
+
+    for(int i = 0; i < n1; ++i) {
+        cin >> a[i];
+    }
+    
+    for(int i = 0; i < n2; ++i) {
+        cin >> b[i];
+    }
+
+    int equals = 1;
+
+    for(int i = 0; i < n1; ++i) {
+        if(a[i] == b[0]) {
+            if(n1 - i >= n2) {
+                for(int j = 0; j < n2; ++j) {
+                    if(b[j] != a[i + j]) {
+                        equals = 0;
+                        break;
+                    }
+                }
+            }
+            else {
+                equals = 0;
+                break;
+            }
+        }
+    }
+
+    if(equals == 1) {
+        cout << "Yes";
+    }
+    else {
+        cout << "No";
+    }
+    return 0;
+}
