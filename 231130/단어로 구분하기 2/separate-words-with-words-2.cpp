fac6bd9d23ce033672b0103496232559ff66b4c0
CunZy <140526291+CunZy@users.noreply.github.com>
2023-11-30 13:45:33 +0900

    Title: 단어로 구분하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231130/단어로 구분하기 2/separate-words-with-words-2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string a[10];
+
+    for(int i = 0; i < 10; ++i) {
+        cin >> a[i];
+    }
+
+    for(int i = 0; i < 10; ++i) {
+        if((i + 1) % 2 == 1) {
+            cout << a[i] << endl;
+        }
+    }
+    return 0;
+}
