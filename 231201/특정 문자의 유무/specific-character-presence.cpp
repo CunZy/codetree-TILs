53b07ba7ec0b1a7b38dab614a4abfb3f56f95a38
CunZy <140526291+CunZy@users.noreply.github.com>
2023-12-01 11:49:46 +0900

    Title: 특정 문자의 유무, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231201/특정 문자의 유무/specific-character-presence.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string a;
+    cin >> a;
+
+    if(a.find("ee") != string::npos) {
+        cout << "Yes" << " ";
+    }
+    else {
+        cout << "No" << " ";
+    }
+
+    if(a.find("ab") != string::npos) {
+        cout << "Yes" << " ";
+    }
+    else {
+        cout << "No" << " ";
+    }
+    return 0;
+}
