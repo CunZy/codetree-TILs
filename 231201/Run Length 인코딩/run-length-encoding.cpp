8badfe008faa1edaa7278e538fcbcbd55230c507
CunZy <140526291+CunZy@users.noreply.github.com>
2023-12-01 11:20:53 +0900

    Title: Run Length 인코딩, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231201/Run Length 인코딩/run-length-encoding.cpp
+++ 231201/Run Length 인코딩/run-length-encoding.cpp
@@ -5,42 +5,67 @@ using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
     string a;
+    char d[2000];
+    int e[2000];
+    char b;
+    int c;
 
     cin >> a;
 
-    char b = a[0];
-    int c = 1;
-
-    for(int i = 0; i < a.length(); ++i) {
-        if(b != a[i]) {
-            ++c;
-            b = a[i];
-        }
-    }
-
-    cout << c * 2 << endl;
-
+    int t = 0;
     b = a[0];
     c = 0;
     for(int i = 0; i < a.length(); ++i) {
-        if(b == a[i]) {
-            ++c;
+        if(b != a[i]) {
+            d[t] = b;
+            e[t] = c;
+            ++t;
+            c = 1;
+            b = a[i];
         }
-        
-        if(i == a.length() - 1) {
+        else if(i == a.length() - 1) {
             if(b == a[i]) {
-                cout << b << c;
+                ++c;
+                d[t] = b;
+                e[t] = c;
+                ++t;
             }
             else {
-                cout << b << c;
-                cout << a[i] << 1;
+                d[t] = b;
+                e[t] = c;
+                ++t;
+                c = 1;
+                d[t] = a[i];
+                e[t] = c;
+                ++t;
             }
         }
-        else if(b != a[i]) {
-            cout << b << c;
-            b = a[i];
-            c = 1;
+        else if(b == a[i]) {
+            ++c;
         }
     }
+
+    int r = 0;
+    for(int i = 0; i < t; ++i) {
+        ++r;
+        while(true) {
+            int q = e[i];
+            if(q / 10 != 0) {
+                q = e[i] / 10;
+                ++r;
+            }
+            else {
+                ++r;
+                break;
+            }
+        }
+    }
+
+    cout << r << endl;
+    for(int i = 0; i < t; ++i) {
+        cout << d[i] << e[i];
+    }
+
+
     return 0;
 }
