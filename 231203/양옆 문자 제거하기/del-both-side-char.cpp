5d14c413be174769def600404d4e9136edd9396d
CunZy <140526291+CunZy@users.noreply.github.com>
2023-12-03 11:16:21 +0900

    Title: 양옆 문자 제거하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231203/양옆 문자 제거하기/del-both-side-char.cpp
+++ 231203/양옆 문자 제거하기/del-both-side-char.cpp
@@ -7,9 +7,13 @@ int main() {
     string a;
     cin >> a;
 
-    a.erase(1, 1);
-    a.erase(a.length() - 2, 1);
+    // a.erase(1, 1);
+    // a.erase(a.length() - 2, 1);
 
-    cout << a;
+    string b = a.substr(0, 1);
+    string c = a.substr(2, a.length() - 4);
+    string d = a.substr(a.length() - 1);
+    cout << b + c + d;
+
     return 0;
 }
