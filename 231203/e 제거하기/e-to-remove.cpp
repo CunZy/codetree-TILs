fcfdd781db2eb2153fb005a4734eee33cf30029b
CunZy <140526291+CunZy@users.noreply.github.com>
2023-12-03 12:06:19 +0900

    Title: e 제거하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231203/e 제거하기/e-to-remove.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string a;
+    cin >> a;
+    for(int i = 0; i < a.length(); ++i) {
+        if(a[i] == 'e') {
+            a.erase(i, 1);
+            break;
+        }
+    }
+
+    cout << a;
+    return 0;
+}
