d4d9acab5905c6d14a9ebf7c7586d36372f16b40
CunZy <140526291+CunZy@users.noreply.github.com>
2023-12-04 10:06:34 +0900

    Title: 그 전 알파벳, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231204/그 전 알파벳/before-the-alphabet.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    char a;
+    cin >> a;
+
+    if(a == 'a') {
+        cout << 'z';
+    }
+    else {
+        cout << (char)(a - 1);
+    }
+    return 0;
+}
