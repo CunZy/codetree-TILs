2c58fa8643f40bced28a47445aef489e2eede7d5
CunZy <140526291+CunZy@users.noreply.github.com>
2023-12-04 09:53:26 +0900

    Title: 아스키코드표 맞추기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231204/아스키코드표 맞추기/chart-of-ASCII.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    for(int i = 0; i < 5; ++i) {
+        int a;
+        cin >> a;
+        cout << (char)a << " ";
+    }
+
+    return 0;
+}
