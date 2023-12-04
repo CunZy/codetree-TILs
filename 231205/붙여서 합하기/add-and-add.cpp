eacf3f585c510e2f442ad284e3010b7d8b09235c
CunZy <140526291+CunZy@users.noreply.github.com>
2023-12-05 08:59:28 +0900

    Title: 붙여서 합하기, Time: 15ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231205/붙여서 합하기/add-and-add.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string a, b;
+    cin >> a >> b;
+
+    string c = a + b;
+    string d = b + a;
+
+    cout << stoi(c) + stoi(d) << endl;
+    return 0;
+}
