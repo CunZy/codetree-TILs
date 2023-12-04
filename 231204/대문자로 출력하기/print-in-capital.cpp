c6385b421216eb530043b1468e58ffe215ba17e7
CunZy <140526291+CunZy@users.noreply.github.com>
2023-12-04 10:12:47 +0900

    Title: 대문자로 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231204/대문자로 출력하기/print-in-capital.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    string a;
+    cin >> a;
+
+    for(int i = 0; i < a.length(); ++i) {
+        if(a[i] >= 'a' && a[i] <= 'z') {
+            cout << (char)toupper(a[i]);
+        }
+        else if(a[i] >= 'A' && a[i] <= 'Z') {
+            cout << a[i];
+        }
+    }
+    return 0;
+}
