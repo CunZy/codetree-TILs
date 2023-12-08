c5afa62bc39c7cf8dbb3f83e7623d4bc0b76ef30
CunZy <140526291+CunZy@users.noreply.github.com>
2023-12-08 16:48:47 +0900

    Title: 짝수이면서 합이 5의 배수인 수, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231208/짝수이면서 합이 5의 배수인 수/an-even-number-with-a-multiple-of-5-in-the-sum.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+bool IsTrue(int n) {
+    int a = n / 10;
+    int b = n % 10;
+
+    if((a + b) % 5 == 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+    if(IsTrue(n)) {
+        cout << "Yes";
+    }
+    else {
+        cout << "No";
+    }
+    return 0;
+}
