63def7beda4e5a2a1ff73472f0dda5adfe3beda0
CunZy <140526291+CunZy@users.noreply.github.com>
2023-12-10 16:36:25 +0900

    Title: 사칙연산 함수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231210/사칙연산 함수/quadratic-operations-function.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int Plus(int a, int b) {
+    return a + b;
+}
+
+int Minus(int a, int b) {
+    return a - b;
+}
+
+int Multi(int a, int b) {
+    return a * b;
+}
+
+int Div(int a, int b) {
+    return a / b;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b;
+    char c;
+
+    cin >> a >> c >> b;
+
+    if(c == '+') cout << a << " " << c << " " << b << " = " << Plus(a, b);
+    else if(c == '-') cout << a << " " << c << " " << b << " = " << Minus(a, b);
+    else if(c == '*') cout << a << " " << c << " " << b << " = " << Multi(a, b);
+    else if(c == '/') cout << a << " " << c << " " << b << " = " << Div(a, b);
+    else {
+        cout << "false";
+    }
+    return 0;
+}
