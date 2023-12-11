364ee7d2b80f068f2036129d6cabcd4ccc692534
CunZy <140526291+CunZy@users.noreply.github.com>
2023-12-11 19:58:02 +0900

    Title: 2021년 날짜의 유무, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231211/2021년 날짜의 유무/with-or-without-2021.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+
+bool IsMonth(int a) {
+    if(a <= 0 || a > 12) {
+        return false;
+    }
+    return true;
+}
+
+bool IsDay(int a, int b) {
+    if(IsMonth(a)) {
+        if(a == 2 && b <= 28) {
+            return true;
+        }
+        if(a < 8) {
+            if(a % 2 == 1) {
+                if(b <= 31) {
+                    return true;
+                }
+            }
+            else {
+                if(b <= 30) {
+                    return true;
+                }
+            }
+        }
+        else if(a >= 8) {
+            if(a % 2 == 0) {
+                if(b <= 31) {
+                    return true;
+                }
+            }
+            else {
+                if(b <= 30) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    return false;
+}
+
+bool Exist(int a, int b) {
+    return IsMonth(a) && IsDay(a, b);
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int m, d;
+    cin >> m >> d;
+
+    if(Exist(m, d)) {
+        cout << "Yes";
+    }
+    else {
+        cout << "No";
+    }
+    return 0;
+}
