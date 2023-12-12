4d0ccd6b24d6e1fa74da14469701d2642f71ded2
CunZy <140526291+CunZy@users.noreply.github.com>
2023-12-12 12:23:40 +0900

    Title: 2개 이상의 알파벳, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231212/2개 이상의 알파벳/more-than-one-alphabet.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool IsTrue(string &a) {
+    int w = 0;
+    char c = a[0];
+    for(int i = 1; i < a.length(); ++i) {
+        if(c != a[i]) {
+            ++w;
+            c = a[i];
+        }
+    }
+
+    if(c >= 2) {
+        return true;
+    }
+    return false;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string a;
+    cin >> a;
+
+    if(IsTrue(a)) {
+        cout << "Yes";
+    }
+    else {
+        cout << "No";
+    }
+    return 0;
+}
