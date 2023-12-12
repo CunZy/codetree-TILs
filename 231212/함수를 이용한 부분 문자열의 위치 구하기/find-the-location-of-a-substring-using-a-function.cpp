504cc939cf10a565109a72c6ded9f91e0a644c4e
CunZy <140526291+CunZy@users.noreply.github.com>
2023-12-12 12:54:58 +0900

    Title: 함수를 이용한 부분 문자열의 위치 구하기, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

--- 231212/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.cpp
+++ 231212/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.cpp
@@ -1,38 +1,31 @@
 #include <iostream>
 using namespace std;
 
-string a;
+string a, b;
 
-int Witch(string &b) {
-
-    bool t = false;
-    int e = 0;
-    for(int i = 0; i < a.length(); ++i) {
-        if(t == false) {
-            for(int j = 0; j < b.length(); ++j) {
-                if(a[i + j] != b[j]) {
-                    break;
-                }
-                if(j == b.length() - 1) {
-                    t = true;
-                    e = i;
-                }
-            }
+bool IsSub(int start) {
+    for(int j = 0; j < b.length(); ++j) {
+        if(a[start + j] != b[j]) {
+            return false;
         }
     }
-    if(t) {
-        return e;
-    }   
-    else {
-        return -1;
-    } 
+
+    return true;
+}
+
+int Find() {
+    for(int i = 0; i < a.length(); ++i) {
+        if(IsSub(i)) {
+            return i;
+        }
+    }
+
+    return -1;
 }
 
 int main() {
-    // 여기에 코드를 작성해주세요.
-    string b;
     cin >> a >> b;
 
-    cout << Witch(b);
+    cout << Find();
     return 0;
 }
