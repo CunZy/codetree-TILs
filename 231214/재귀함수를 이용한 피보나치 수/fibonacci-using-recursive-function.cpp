2b9a961084f7817c8838fd57346fc7ad686d3882
CunZy <140526291+CunZy@users.noreply.github.com>
2023-12-14 15:38:41 +0900

    Title: 재귀함수를 이용한 피보나치 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231214/재귀함수를 이용한 피보나치 수/fibonacci-using-recursive-function.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int Fibonacci(int n) {
+    if(n == 1 || n == 2) {
+        return 1;
+    }
+    return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+    cout << Fibonacci(n);
+    return 0;
+}
