9f05b560dd98ff46903bc2fb13c7d848e983b9eb
CunZy <140526291+CunZy@users.noreply.github.com>
2023-12-14 15:42:09 +0900

    Title: Factorial, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231214/Factorial/factorial.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int Factorial(int n) {
+    if(n <= 1) {
+        return 1;
+    }
+    return Factorial(n - 1) * n;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+    cout << Factorial(n);
+    return 0;
+}
