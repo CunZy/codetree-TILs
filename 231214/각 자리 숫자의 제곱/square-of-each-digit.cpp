2c3c26cc86f4a3f5619d04c26cc09bc3a8af5ef0
CunZy <140526291+CunZy@users.noreply.github.com>
2023-12-14 11:09:39 +0900

    Title: 각 자리 숫자의 제곱, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231214/각 자리 숫자의 제곱/square-of-each-digit.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int Sum(int n) {
+    if(n < 10) {
+        return n * n;
+    }
+
+    return Sum(n / 10) + ((n % 10) * (n % 10));
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+    cout << Sum(n);
+    return 0;
+}
