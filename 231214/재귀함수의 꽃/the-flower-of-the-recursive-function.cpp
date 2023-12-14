a6e59c5502e8ce53d1a0c084cfe45d249fffe860
CunZy <140526291+CunZy@users.noreply.github.com>
2023-12-14 10:47:30 +0900

    Title: 재귀함수의 꽃, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231214/재귀함수의 꽃/the-flower-of-the-recursive-function.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void Print(int n) {
+    if(n == 0) {
+        return;
+    }
+    cout << n << " ";
+    Print(n - 1);
+    cout << n << " ";
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+    Print(n);
+    return 0;
+}
