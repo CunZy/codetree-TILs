33fbe6f457d04672318ab246780343d7b63b6bf7
CunZy <140526291+CunZy@users.noreply.github.com>
2023-12-14 15:46:53 +0900

    Title: 홀수 짝수에 따른 출력값, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 231214/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.cpp
+++ 231214/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.cpp
@@ -5,11 +5,7 @@ int Sol(int n) {
     if(n == 1) {
         return 1;
     }
-    return Sol(n - 2) + n;
-}
-
-int Even(int n) {
-    if(n == 2) {
+    else if(n == 2) {
         return 2;
     }
     return Sol(n - 2) + n;
@@ -19,11 +15,7 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int n;
     cin >> n;
-    if(n % 2 == 0) {
-        cout << Even(n);
-    }
-    else {
-        cout << Sol(n);
-    }
+    
+    cout << Sol(n);
     return 0;
 }
