af70f5202dff3f9dd624566312184d73283a8c9d
CunZy <140526291+CunZy@users.noreply.github.com>
2024-01-21 17:05:11 +0900

    Title: 왔다 갔던 구역 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240121/왔다 갔던 구역 2/area-been-to-and-from2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+    int a[2001] = {};
+    int t = 1000;
+
+    for(int i = 0; i < n; ++i) {
+        int x;
+        char y;
+        cin >> x >> y;
+
+        int r;
+        if(y == 'R') {
+            r = t + x;
+
+            for(int j = t; j < r; ++j) {
+                a[j]++;
+            }
+        }
+        else if(y == 'L') {
+            r = t - x;
+
+            for(int j = t; j > r; --j) {
+                a[j]++;
+            }
+        }
+        t = r;
+    }
+
+    int cnt = 0;
+
+    for(int i = 0; i < 2001; ++i) {
+        if(a[i] >= 2) {
+            cnt++;
+        } 
+    }
+
+    cout << cnt;
+    return 0;
+}
