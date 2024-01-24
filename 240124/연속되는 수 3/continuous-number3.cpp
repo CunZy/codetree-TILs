663b42ea35ea8906571681815909e6681316c7d5
CunZy <140526291+CunZy@users.noreply.github.com>
2024-01-24 15:57:51 +0900

    Title: 연속되는 수 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240124/연속되는 수 3/continuous-number3.cpp
+++ 240124/연속되는 수 3/continuous-number3.cpp
@@ -11,12 +11,15 @@ int main() {
         cin >> a[i];
     }
 
-    int cnt = 0;
+    int cnt = 1;
     int max = 0;
 
     for(int i = 0; i < n; ++i) {
         if(i >= 1 && (a[i] * a[i - 1] >= 0)) {
             cnt++;
+            if(max < cnt) {
+                max = cnt;
+            }
         }
         else {
             if(max < cnt) {
