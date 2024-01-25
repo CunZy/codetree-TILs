053adf4cfe6352440317ca2d135fbae37131a711
CunZy <140526291+CunZy@users.noreply.github.com>
2024-01-25 14:53:20 +0900

    Title: 벌금은 누구에게, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240125/벌금은 누구에게/who-will-pay.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, m, k;
+    int a[100] = {};
+
+    cin >> n >> m >> k;
+
+    int ans = -1;
+
+    for(int i = 0; i < m; ++i) {
+        int num;
+        cin >> num;
+
+        a[num - 1]++;
+        if(a[num - 1] >= k) {
+            ans = num;
+        }
+    }
+
+    cout << ans;
+    return 0;
+}
