9b4f378951d8353e262d48d6e4fae7df5475e78e
CunZy <140526291+CunZy@users.noreply.github.com>
2024-04-02 23:47:23 +0900

    Title: 문자열에서 문자 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240402/문자열에서 문자 출력/print-chars-from-word.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string a;
+    cin >> a;
+
+    for(int i = 0; a[i] != '\0'; ++i) {
+        cout << a[i] << endl;
+    }
+    return 0;
+}
