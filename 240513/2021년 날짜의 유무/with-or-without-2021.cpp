fb4c4dff3f69151276d05fdd2d1dcffd601e4a2d
CunZy <140526291+CunZy@users.noreply.github.com>
2024-05-13 23:11:24 +0900

    Title: 2021년 날짜의 유무, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240513/2021년 날짜의 유무/with-or-without-2021.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int month[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+bool IsTrue(int m, int d) {
+    if(month[m - 1] >= d) {
+        return true;
+    }
+    return false;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int m, d;
+    cin >> m >> d;
+
+    if(IsTrue(m, d)) {
+        cout << "Yes";
+    }
+    else {
+        cout << "No";
+    }
+    return 0;
+}
