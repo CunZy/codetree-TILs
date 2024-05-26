0a6c098f14547fc7e963b0efd2d7076e8edb1603
CunZy <140526291+CunZy@users.noreply.github.com>
2024-05-26 14:43:57 +0900

    Title: 반복 출력하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240526/반복 출력하기 2/repeated-output-2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void Print(int n) {
+    if(n == 0) {
+        return;
+    }
+    Print(n - 1);
+    cout << "HelloWorld" << endl;    
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+    Print(n);
+    return 0;
+}
