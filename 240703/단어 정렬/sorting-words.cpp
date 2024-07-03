1fe52c31b0929ae807ced235223626314f6e98ea
CunZy <140526291+CunZy@users.noreply.github.com>
2024-07-03 22:17:39 +0900

    Title: 단어 정렬, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240703/단어 정렬/sorting-words.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string a[100] = {};
+    int n;
+
+    cin >> n;
+
+    for(int i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
+
+    sort(a, a + n);
+
+    for(int i = 0; i < n; ++i) {
+        cout << a[i] << endl;
+    }
+    return 0;
+}
