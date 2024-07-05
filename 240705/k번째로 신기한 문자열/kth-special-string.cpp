9fac92b55ca6580e4ab727d74fb0031d70f28d86
CunZy <140526291+CunZy@users.noreply.github.com>
2024-07-05 17:17:07 +0900

    Title: k번째로 신기한 문자열, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240705/k번째로 신기한 문자열/kth-special-string.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, k;
+    string t;
+    cin >> n >> k >> t;
+
+    string w[100] = {};
+    int count = 0;
+    for(int i = 0; i < n; ++i) {
+        string a;
+        cin >> a;
+        bool e = true;
+        for(int j = 0; j < t.length(); ++j) {
+            if(a[j] != t[j]) {
+                e = false;
+            }
+        }
+
+        if(e) {
+            w[i] = a;
+            ++count;
+        }
+    }
+
+    sort(w, w + count);
+
+    cout << w[k - 1] << endl;
+    return 0;
+}
