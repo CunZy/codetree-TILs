6f82e43d89236f2e7fb2556b4ad5a97c21ff5dbe
CunZy <140526291+CunZy@users.noreply.github.com>
2024-07-15 16:03:42 +0900

    Title: Date to Date, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240715/Date to Date/date-to-date.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+
+    int m[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    // solve 1
+
+    int total = 0;
+
+    for(int i = a; i <= c; ++i) {
+        if(i == a) {
+            total += m[a] - b + 1;
+        }
+        else if(i == c) {
+            total += d;
+        }
+        else {
+            total += m[i];
+        }
+    }
+
+    cout << total;
+    return 0;
+}
