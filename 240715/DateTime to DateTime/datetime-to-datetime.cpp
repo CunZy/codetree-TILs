25799c6e59690fcd0e09df2266e21283eb758791
CunZy <140526291+CunZy@users.noreply.github.com>
2024-07-15 16:31:30 +0900

    Title: DateTime to DateTime, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240715/DateTime to DateTime/datetime-to-datetime.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+bool check(int a, int b) {
+    if(a < 11) {
+        return false;
+    }
+    else {
+        if(b < 11) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int cal(int a, int b, int c) {
+    int total = 0;
+    int q = 11, w = 11, e = 11;
+    while(true) {
+        if(q == a && w == b && e == c) {
+            return total;
+        }
+        ++e;
+        total++;
+
+        if(e == 60) {
+            ++w;
+            e = 0;
+        }
+        if(w == 24) {
+            ++q;
+            w = 0;
+        }
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    if(check(b, c)) {
+        cout << cal(a, b, c);
+    }
+    else {
+        cout << -1 << endl;
+    }
+    return 0;
+}
