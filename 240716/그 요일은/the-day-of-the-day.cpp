46f459dbee0439cf36b0cb7fc1e1aa0e4c6882a9
CunZy <140526291+CunZy@users.noreply.github.com>
2024-07-16 16:38:37 +0900

    Title: 그 요일은, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240716/그 요일은/the-day-of-the-day.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+int dayw(int a, int b) {
+    int m[13] = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    int total = 0;
+
+    for(int i = 1; i < a; ++i) {
+        total += m[i];
+    }
+
+    total += b;
+
+    return total;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, c, d;
+    string days[7] = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+    
+    string A;
+    cin >> a >> b >> c >> d;
+    cin >> A;
+
+    int dayn;
+    for(int i = 0; i < 7; ++i) {
+        if(days[i] == A) {
+            dayn = i;
+            break;
+        }
+    }
+
+    int day = dayw(c, d) - dayw(a, b) + 1;
+
+    int time = 0;
+
+    day -= dayn;
+
+    while(day >= 0) {
+        ++time;
+        day -= 7;
+    }
+
+    cout << time << endl;
+    
+
+
+
+    return 0;
+}
