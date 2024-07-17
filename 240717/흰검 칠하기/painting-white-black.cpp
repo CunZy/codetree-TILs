9dd65374cb1eac59168ea509ced6c5e3cbca9cff
CunZy <140526291+CunZy@users.noreply.github.com>
2024-07-17 18:03:51 +0900

    Title: 흰검 칠하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240717/흰검 칠하기/painting-white-black.cpp
+++ 240717/흰검 칠하기/painting-white-black.cpp
@@ -3,12 +3,14 @@ using namespace std;
 
 class Tile {
     public:
-        int n;
+        int n1;
+        int n2;
         int color;
 
-        Tile(int a = 0, int b = 0) {
-            n = a;
-            color = b;
+        Tile(int a = 0, int b = 0, int c = 0) {
+            n1 = a;
+            n2 = b;
+            color = c;
         }
 };
 
@@ -34,8 +36,11 @@ int main() {
 
         if(y == 'L') {
             for(int j = 0; j < x; ++j) {
-                tile[here].n += 1;
-                if(tile[here].n >= 4) {
+                tile[here].n1 += 1;
+                if(tile[here].n1 > 2 && tile[here].n2 > 2) {
+                    continue;
+                }
+                else if(tile[here].n1 == 2 && tile[here].n2 == 2) {
                     tile[here].color = 3;
                 }
                 else {
@@ -49,8 +54,11 @@ int main() {
         }
         else if(y == 'R') {
             for(int j = 0; j < x; ++j) {
-                tile[here].n += 1;
-                if(tile[here].n >= 4) {
+                tile[here].n2 += 1;
+                if(tile[here].n1 > 2 && tile[here].n2 > 2) {
+                    continue;
+                }
+                else if(tile[here].n1 == 2 && tile[here].n2 == 2) {
                     tile[here].color = 3;
                 }
                 else {
@@ -78,20 +86,6 @@ int main() {
             ++g;
         }
     }
-
-    // for(int i = 0; i < 200; ++i) {
-    //     if(tile[i].n > 0) {
-    //         cout << tile[i].color << " ";
-    //     }
-    // }
-    // cout << endl;
-
-    // for(int i = 0; i < 200; ++i) {
-    //     if(tile[i].n > 0) {
-    //         cout << tile[i].n << " ";
-    //     }
-    // }
-    // cout << endl;
 
     cout << w << " " << b << " " << g << endl;
 
