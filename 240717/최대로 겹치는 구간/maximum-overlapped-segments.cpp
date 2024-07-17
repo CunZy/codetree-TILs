802a8f2ade03973202609bfdb0547e36d22a0c88
CunZy <140526291+CunZy@users.noreply.github.com>
2024-07-17 16:46:29 +0900

    Title: 최대로 겹치는 구간, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240717/최대로 겹치는 구간/maximum-overlapped-segments.cpp
+++ 240717/최대로 겹치는 구간/maximum-overlapped-segments.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int a[100] = {};
+    int a[200] = {};
 
     int n;
     cin >> n;
@@ -12,13 +12,16 @@ int main() {
         int b, c;
         cin >> b >> c;
 
+        b += 100;
+        c += 100;
+
         for(int j = b - 1; j < c - 1; ++j) {
             a[j] += 1;
         }
     }
 
     int max = a[0];
-    for(int i = 0; i < 100; i++) {
+    for(int i = 0; i < 200; i++) {
         if(max < a[i]) {
             max = a[i];
         }
