b3848ac366e7dfbdb1b466a8fe2ac9209a063a33
CunZy <140526291+CunZy@users.noreply.github.com>
2024-07-22 17:58:58 +0900

    Title: 색종이의 총 넓이, Time: 14ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240722/색종이의 총 넓이/the-total-area-of-colored-paper..cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+    int a[201][201] = {};
+
+    int x, y;
+
+    for(int i = 0; i < n; ++i) {
+        cin >> x >> y;
+
+        for(int j = 0; j < 8; ++j) {
+            for(int k = 0; k < 8; ++k) {
+                a[x + j][y + k] = 1;
+            }
+        }
+    }
+
+    int h = 0;
+
+    for(int i = 0; i < 201; ++i) {
+        for(int j = 0; j < 201; ++j) {
+            h += a[i][j];
+        }
+    }
+
+    cout << h << endl;
+    return 0;
+}
