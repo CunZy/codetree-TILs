6bee84b584211b47e616657650f3d06009a9ba32
CunZy <140526291+CunZy@users.noreply.github.com>
2024-07-23 17:25:23 +0900

    Title: 계속 중첩되는 사각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240723/계속 중첩되는 사각형/continuously-overlapping-squares.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    int a[201][201] = {};
+
+    for(int i = 0; i < n; ++i) {
+        int x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+
+        x1 += 100;
+        y1 += 100;
+        x2 += 100;
+        y2 += 100;
+
+
+        for(int j = x1; j < x2; ++j) {
+            for(int k = y1; k < y2; ++k) {
+                if(i % 2 == 0) {
+                    a[j][k] = 1;
+                }
+                else {
+                    a[j][k] = 2;
+                }
+            }
+        }
+    }
+
+    int h = 0;
+
+    for(int i = 0; i < 201; ++i) {
+        for(int j = 0; j < 201; ++j) {
+            if(a[i][j] == 2) {
+                h += 1;
+            }
+        }
+    }
+
+    cout << h << endl;
+    return 0;
+}
