a4129c31b92dfaeaa19f325210402ce911b7d2a7
CunZy <140526291+CunZy@users.noreply.github.com>
2024-07-30 18:05:41 +0900

    Title: 문자에 따른 명령 2, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240730/문자에 따른 명령 2/text-based-commands2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int x = 0, y = 0;
+    int dx[4] = {0, 1, 0, -1};
+    int dy[4] = {1, 0, -1, 0};
+    int dir = 0;
+
+    string a;
+    cin >> a;
+
+    for(int i = 0; i < a.length(); ++i) {
+
+        if(a[i] == 'L') {
+            dir = (dir + 3) % 4;
+        }
+        else if(a[i] == 'R') {
+            dir = (dir + 1) % 4;
+        }
+        else if(a[i] == 'F') {
+            x += dx[dir];
+            y += dy[dir];
+        }
+    }
+
+    cout << x << " " << y << endl;
+    return 0;
+}
