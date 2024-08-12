a64f7b421169022e5e4684bfd7486f105838da81
CunZy <140526291+CunZy@users.noreply.github.com>
2024-08-12 15:51:10 +0900

    Title: 거울에 레이저 쏘기 2, Time: 1051ms, Memory: 1MB, Status: Time Limit Exceed - CodeTree

---
+++ 240812/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.cpp
@@ -0,0 +1,114 @@
+#include <iostream>
+using namespace std;
+
+bool IsRange(int a, int b, int n) {
+    return a >= 0 && a < n && b >= 0 && b < n;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+    char a[1000][1000] = {};
+
+    for(int i = 0; i < n; ++i) {
+        string mirror;
+        cin >> mirror;
+
+        for(int j = 0; j < n; ++j) {
+            a[i][j] = mirror[j];
+        }
+    }
+
+    // for(int i = 0; i < n; ++i) {
+    //     for(int j = 0; j < n; ++j) {
+    //         cout << a[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    int k;
+    cin >> k;
+
+    int x, y;
+
+    int sx = 0, sy = 0;
+    int dsx[4] = {0, 1, 0, -1};
+    int dsy[4] = {1, 0, -1, 0};
+    int sd = 0;
+
+    for(int i = 0; i < k; ++i) {
+        if((i + 1) % n == 0) {
+            sx = sx + dsx[sd];
+            sy = sy + dsy[sd];
+            sd = (sd + 1) % 4;
+        }
+        else if(i % n == 0) {
+            continue;
+        }
+        else {
+            sx = sx + dsx[sd];
+            sy = sy + dsy[sd];
+        }
+    }
+
+    // cout << sx << " " << sy << endl;
+
+    // // /
+    // int dx1[4] = {1, -1, 0, 0}; // 아래, 위, 오른, 왼
+    // int dy1[4] = {0, 0, 1, -1}; // 0   , 1 , 2 ,   3
+
+    // // \ /
+    // int dx2[4] = {1, -1, 0, 0}; // 아래, 위, 왼, 오른
+    // int dy2[4] = {0, 0, -1, 1}; //  0 ,  1,  2,   3
+
+    int dx[4] = {1, 0, -1, 0}; // 아래, 왼, 위, 오른
+    int dy[4] = {0, -1, 0, 1}; //   0,  1,  2,   3
+
+    int dir;
+
+    if(k <= n) {
+        dir = 0;
+    }
+    else if(k <= 2 * n) {
+        if(a[sx][sy] == '/') {
+            dir = 3;
+        }
+        else if(a[sx][sy] == '\\') {
+            dir = 2;
+        }
+    }
+    else if(k <= 3 * n) {
+        dir = 1;
+    }
+    else {
+        if(a[sx][sy] == '/') {
+            dir = 2;
+        }
+        else if(a[sx][sy] == '\\') {
+            dir = 3;
+        }
+    }
+
+    // cout << dir << endl;
+
+    int count = 0;
+
+    while(IsRange(sx, sy, n)) {
+        if(a[sx][sy] == '/') {
+            dir = (dir + 3) % 4;
+            sx += dx[dir];
+            sy += dy[dir];
+        }
+        else if(a[sx][sy] == '\\') {
+            dir = (dir + 1) % 4;
+            sx += dx[dir];
+            sy += dy[dir];
+        }
+        count++;
+    }
+
+    cout << count << endl;
+    return 0;
+}
