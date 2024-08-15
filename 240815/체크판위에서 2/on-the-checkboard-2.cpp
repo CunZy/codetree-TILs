9c411210985da7646b94e213c9cae41719b909a4
CunZy <140526291+CunZy@users.noreply.github.com>
2024-08-15 14:05:00 +0900

    Title: 체크판위에서 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240815/체크판위에서 2/on-the-checkboard-2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int r, c;
+    cin >> r >> c;
+
+    char a[15][15] = {};
+
+    for(int i = 0; i < r; ++i) {
+        for(int j = 0; j < c; ++j) {
+            cin >> a[i][j];
+        }
+    }
+
+    int cnt = 0;
+    int x = 0, y = 0;
+    
+    for(int i = 1; i < r - 2; ++i) {
+        for(int j = 1; j < c - 2; ++j) {
+            if(a[i][j] == 'B') {
+                for(int k = i + 1; k < r - 1; ++k) {
+                    for(int p = j + 1; p < c - 1; ++p) {
+                        if(a[k][p] == 'W') {
+                            ++cnt;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    cout << cnt << endl;
+    return 0;
+}
