175ea653f54e1518a16ed1a6a189a55d5b029b3f
CunZy <140526291+CunZy@users.noreply.github.com>
2024-08-15 14:05:59 +0900

    Title: 체크판위에서 2, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240815/체크판위에서 2/on-the-checkboard-2.cpp
+++ 240815/체크판위에서 2/on-the-checkboard-2.cpp
@@ -16,20 +16,23 @@ int main() {
 
     int cnt = 0;
     int x = 0, y = 0;
-    
-    for(int i = 1; i < r - 2; ++i) {
-        for(int j = 1; j < c - 2; ++j) {
-            if(a[i][j] == 'B') {
-                for(int k = i + 1; k < r - 1; ++k) {
-                    for(int p = j + 1; p < c - 1; ++p) {
-                        if(a[k][p] == 'W') {
-                            ++cnt;
+
+    if(a[r - 1][c - 1] != 'W') {
+        for(int i = 1; i < r - 2; ++i) {
+            for(int j = 1; j < c - 2; ++j) {
+                if(a[i][j] == 'B') {
+                    for(int k = i + 1; k < r - 1; ++k) {
+                        for(int p = j + 1; p < c - 1; ++p) {
+                            if(a[k][p] == 'W') {
+                                ++cnt;
+                            }
                         }
                     }
                 }
             }
         }
     }
+    
 
     cout << cnt << endl;
     return 0;
