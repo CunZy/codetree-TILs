8ea2ac36686d223c27f8cbd8660ece1c7d9c5cbe
CunZy <140526291+CunZy@users.noreply.github.com>
2024-08-21 16:51:30 +0900

    Title: 최고의 13위치 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240821/최고의 13위치 2/best-place-of-13-2.cpp
+++ 240821/최고의 13위치 2/best-place-of-13-2.cpp
@@ -18,8 +18,11 @@ int main() {
 
     for(int i = 0; i < n; ++i) {
         for(int j = 0; j < n - 2; ++j) {
-            for(int k = i + 1; k < n; ++k) {
+            for(int k = i; k < n; ++k) {
                 for(int l = 0; l < n - 2; ++l) {
+                    if(k == i && j - 2 <= l && l <= j + 2) {
+                        continue;
+                    }
                     if(max < a[i][j] + a[i][j + 1] + a[i][j + 2] + a[k][l] + a[k][l + 1] + a[k][l + 2]) {
                         max = a[i][j] + a[i][j + 1] + a[i][j + 2] + a[k][l] + a[k][l + 1] + a[k][l + 2];
                     }
