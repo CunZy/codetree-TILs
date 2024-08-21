f5993e467d2c46a98fdbd52eb9bbbdc2a26b01fa
CunZy <140526291+CunZy@users.noreply.github.com>
2024-08-21 16:42:26 +0900

    Title: 최고의 13위치 2, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240821/최고의 13위치 2/best-place-of-13-2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a[20][20] = {};
+    int n;
+    cin >> n;
+
+    for(int i = 0; i < n; ++i) {
+        for(int j = 0; j < n; ++j) {
+            cin >> a[i][j];
+        }
+    }
+
+    int max = INT_MIN;
+
+    for(int i = 0; i < n; ++i) {
+        for(int j = 0; j < n - 2; ++j) {
+            for(int k = i + 1; k < n; ++k) {
+                for(int l = 0; l < n - 2; ++l) {
+                    if(max < a[i][j] + a[i][j + 1] + a[i][j + 2] + a[k][l] + a[k][l + 1] + a[k][l + 2]) {
+                        max = a[i][j] + a[i][j + 1] + a[i][j + 2] + a[k][l] + a[k][l + 1] + a[k][l + 2];
+                    }
+                }
+            }
+        }
+    }
+
+    cout << max << endl;
+    return 0;
+}
