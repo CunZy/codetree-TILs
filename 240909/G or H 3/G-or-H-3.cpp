232cc0a14eef36b998f29f8152ac8a054228e8a0
CunZy <140526291+CunZy@users.noreply.github.com>
2024-09-09 15:17:54 +0900

    Title: G or H 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240909/G or H 3/G-or-H-3.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+    int k;
+    cin >> k;
+
+    char location[10001] = {};
+
+    int ma = INT_MIN;
+
+    for(int i = 0; i < n; ++i) {
+        int loc;
+        char alp;
+
+        cin >> loc >> alp;
+
+        location[loc] = alp;
+        ma = max(loc, ma);
+    }
+
+    int ans = INT_MIN;
+
+    for(int i = 1; i <= ma - k; ++i) {
+        int pre = 0;
+        for(int j = i; j <= i + k; ++j) {
+            if(location[j] == 'G') {
+                pre += 1;
+            }
+            else if(location[j] == 'H') {
+                pre += 2;
+            }
+        }
+
+        // cout << pre << endl;
+
+        ans = max(ans, pre);
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
