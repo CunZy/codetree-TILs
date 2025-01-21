5b11c4e544cd52fae2aa086711a78c5d97c3d82f
CunZy <140526291+CunZy@users.noreply.github.com>
2025-01-21 21:34:25 +0900

    Title: 변수 출력하기, Time: 92ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250121/변수 출력하기/outputing-variables.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a;
+        char b;
+        a = 3;
+        b = 'C';
+
+        System.out.printf("%d...%c", a, b);
+    }
+}
