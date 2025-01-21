f49119ea5fabb3a9f7da70294a0358ddf9c37250
CunZy <140526291+CunZy@users.noreply.github.com>
2025-01-21 17:12:06 +0900

    Title: 두줄 출력, Time: 93ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250121/두줄 출력/print-two-sentences-introduce.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        System.out.print("Hello students!\n");
+        System.out.print("Welcome to LeebrosCode!");
+    }
+}
