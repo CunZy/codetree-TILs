ed946a7c094058f751114a6543f36828a0e9312f
CunZy <140526291+CunZy@users.noreply.github.com>
2025-01-25 15:20:54 +0900

    Title: 변수 값 교체하기 3, Time: 79ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250125/변수 값 교체하기 3/replacing-variable-values-3.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 3, b = 5;
+        int temp = b;
+        b = a;
+        a = temp;
+        System.out.println(a + "\n" + b);
+    }
+}
