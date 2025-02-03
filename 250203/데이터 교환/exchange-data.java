e82c4a52cdee8e42733a340acc72f815b309e5a5
CunZy <140526291+CunZy@users.noreply.github.com>
2025-02-03 16:38:47 +0900

    Title: 데이터 교환, Time: 91ms, Memory: 9MB, Status: Passed - CodeTree

--- 250203/데이터 교환/exchange-data.java
+++ 250203/데이터 교환/exchange-data.java
@@ -3,9 +3,9 @@ public class Main {
         // Please write your code here.
         int a = 5, b = 6, c = 7;
         int t = a;
-        a = b;
-        b = c;
-        c = t;
+        a = c;
+        c = b;
+        b = t;
 
         System.out.printf("%d\n%d\n%d", a, b, c);
     }
