253e8450de1c18ab735242dd7dcbe9f13a5d40eb
CunZy <140526291+CunZy@users.noreply.github.com>
2025-02-04 16:33:23 +0900

    Title: 변수 값 교체하기 6, Time: 95ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250204/변수 값 교체하기 6/replacing-variable-values-6.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 2, b = 5;
+        int t = a;
+        a = b;
+        b = t;
+
+        System.out.printf("%d\n%d", a, b);
+    }
+}
