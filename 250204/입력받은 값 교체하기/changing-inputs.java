4fa2b95e4357a76e178b7e6e440e1f4560a0748c
CunZy <140526291+CunZy@users.noreply.github.com>
2025-02-04 16:52:51 +0900

    Title: 입력받은 값 교체하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 250204/입력받은 값 교체하기/changing-inputs.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt(), b = sc.nextInt();
+
+        int t = a;
+        a = b;
+        b = t;
+
+        System.out.print("%d %d", a, b);
+    }
+}
