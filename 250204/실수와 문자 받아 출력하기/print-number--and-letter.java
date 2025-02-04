81576b17585f387fde5d8798e09ef0bbe5c47c7e
CunZy <140526291+CunZy@users.noreply.github.com>
2025-02-04 18:22:23 +0900

    Title: 실수와 문자 받아 출력하기, Time: 145ms, Memory: 11MB, Status: Passed - CodeTree

--- 250204/실수와 문자 받아 출력하기/print-number--and-letter.java
+++ 250204/실수와 문자 받아 출력하기/print-number--and-letter.java
@@ -8,6 +8,6 @@ public class Main {
         char c = sc.next().charAt(0);
         double a = sc.nextDouble(), b = sc.nextDouble();
 
-        System.out.printf("%c\n%.2f\n%.2f", c, b, a);
+        System.out.printf("%c\n%.2f\n%.2f", c, a, b);
     }
 }
