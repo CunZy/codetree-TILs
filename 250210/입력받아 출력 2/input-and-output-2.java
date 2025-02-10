eddab9923372d7b0f2f015f1a79692417ce9b600
CunZy <140526291+CunZy@users.noreply.github.com>
2025-02-10 14:23:04 +0900

    Title: 입력받아 출력 2, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250210/입력받아 출력 2/input-and-output-2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        Scanner sc = new Scanner(System.in);
+
+        sc.useDelimiter("-");
+
+        String a = sc.next(), b = sc.next();
+
+        System.out.printf("%s%s", a, b);
+    }
+}
