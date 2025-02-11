168701504f75c4cd66fe63ed698a0fde99130fde
CunZy <140526291+CunZy@users.noreply.github.com>
2025-02-11 15:36:11 +0900

    Title: 점수 비교, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250211/점수 비교/score-comparison.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write yur code here.
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt(), b = sc.nextInt();
+
+        int c = sc.nextInt(), d = sc.nextInt();
+
+        if(a > c && b > d) {
+            System.out.print(1);
+        }
+        else {
+            System.out.print(0);
+        }
+    }
+}
