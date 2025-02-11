97dbd5e896d563e75fdd3c687b3c4bd54b42dd97
CunZy <140526291+CunZy@users.noreply.github.com>
2025-02-11 15:59:04 +0900

    Title: 장학금, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 250211/장학금/scholarship.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt(), b = sc.nextInt();
+
+        if(a < 90) {
+            System.out.print(0);
+        }
+
+        if(b < 90) {
+            System.out.print(0);
+        }
+        else if(b < 95) {
+            System.out.print(50000);
+        }
+        else {
+            System.out.print(100000)
+        }
+    }
+}
