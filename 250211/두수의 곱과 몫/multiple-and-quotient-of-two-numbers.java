68be6042173671559356383def9626f180dfa3d6
CunZy <140526291+CunZy@users.noreply.github.com>
2025-02-11 14:46:31 +0900

    Title: 두수의 곱과 몫, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250211/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt(), b = sc.nextInt();
+
+        System.out.printf("%d * %d = %d", a, b, a * b);
+        System.out.printf("\n%d / %d = %d", a, b, a / b);
+        // Please write your code here.
+    }
+}
