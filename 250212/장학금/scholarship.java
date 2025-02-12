eb0bea322b32b56d8fbb2fea17e3c411516f9b71
CunZy <140526291+CunZy@users.noreply.github.com>
2025-02-12 16:55:28 +0900

    Title: 장학금, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250212/장학금/scholarship.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt(), b = sc.nextInt();
+
+        if(a < 90) {
+            System.out.print(0);
+        }
+        else if(b >= 95) {
+            System.out.print(100000);
+        }
+        else if(b >= 90) {
+            System.out.print(50000);
+        }
+        else {
+            System.out.print(0);
+        }
+    }
+}
