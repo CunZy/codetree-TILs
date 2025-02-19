cfb92c4d5bb5688abffca8b2688cdd5d04f05119
CunZy <140526291+CunZy@users.noreply.github.com>
2025-02-19 14:49:45 +0900

    Title: 세 수의 합과 평균 구하기, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

--- 250219/세 수의 합과 평균 구하기/sum-and-mean-of-three-numbers.java
+++ 250219/세 수의 합과 평균 구하기/sum-and-mean-of-three-numbers.java
@@ -6,6 +6,6 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt();
 
-        System.out.printf("%d %d", (a + b + c), (a + b + c) / 3);
+        System.out.printf("%d\n%d", (a + b + c), (a + b + c) / 3);
     }
 }
