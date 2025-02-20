c4cfdde7b64404ca9f67351c905cf95c1e4d224d
CunZy <140526291+CunZy@users.noreply.github.com>
2025-02-20 16:38:22 +0900

    Title: 정수 사이의 배수, Time: 119ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250220/정수 사이의 배수/multiplication-between-numbers.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt(), b = sc.nextInt();
+
+        int c = 0;
+
+        int cnt = 0;
+
+        for(int i = a; i <= b; ++i) {
+            if(i % 5 == 0 || i % 7 == 0) {
+                c += i;
+                cnt++;
+            }
+        }
+
+        System.out.printf("%d %.1f", c, (double)c / cnt);
+    }
+}
