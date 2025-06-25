f8f30bbc7a761c508580fcf3f6426aa610be555b
CunZy <140526291+CunZy@users.noreply.github.com>
2025-06-25 17:51:35 +0900

    Title: 반복 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250625/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+void print() {
+    for(int i = 0; i < N; ++i) {
+        cout << "12345^&*()_" << "\n";
+    }
+}
+
+int main() {
+    cin >> N;
+
+    // Please write your code here.
+    print();
+    return 0;
+}
