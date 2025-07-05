f4fb0abacaebbde3ba5e59a22fa4c6ff56be155f
CunZy <140526291+CunZy@users.noreply.github.com>
2025-07-05 11:15:55 +0900

    Title: 1부터 특정 수까지의 합, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250705/1부터 특정 수까지의 합/sum-from-1-to-a-certain-number.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+int sum(int a) {
+    int t = 0;
+
+    for(int i = 1; i <= a; ++i) {
+        t += i;
+    }
+
+    return t / 10;
+}
+
+int main() {
+    cin >> N;
+
+    // Please write your code here.
+    cout << sum(N) << endl;
+    return 0;
+}
