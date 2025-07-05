463b78f2f27be2c0670300387e43befa52a15292
CunZy <140526291+CunZy@users.noreply.github.com>
2025-07-05 16:10:54 +0900

    Title: 함수를 이용한 윤년 판별, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250705/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int y;
+
+bool isYear(int e) {
+    if(e % 4 != 0) {
+        return false;
+    }
+    if(e % 100 != 0) {
+        return false;
+    }
+    if(e % 400 != 0) {
+        return false;
+    }
+    return true;
+}
+
+int main() {
+    cin >> y;
+
+    // Please write your code here.
+    if(isYear(y)) {
+        cout << "true";
+    }
+    else {
+        cout << "false;"
+    }
+    return 0;
+}
