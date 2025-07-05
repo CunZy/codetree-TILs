40d542ec2b7c3d1d11696aa4552cadfec02269ec
CunZy <140526291+CunZy@users.noreply.github.com>
2025-07-05 17:09:44 +0900

    Title: 두 정수에 대한 연산값, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250705/두 정수에 대한 연산값/operational-values-for-two-integers.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int a, b;
+
+void change(int &n, int &m) {
+    if(n > m) {
+        n += 25;
+        m *= 2;
+    }
+    else {
+        n *= 2;
+        m += 25;
+    }
+}
+
+int main() {
+    cin >> a >> b;
+
+    // Please write your code here.
+    change(a, b);
+    cout << a << " " << b << endl;
+    return 0;
+}
