ae0bd6cb406a66ff96a8ecfb134639b1b87d398e
CunZy <140526291+CunZy@users.noreply.github.com>
2025-07-05 11:08:45 +0900

    Title: 최소공배수 구하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250705/최소공배수 구하기/find-the-least-common-multiple.cpp
+++ 250705/최소공배수 구하기/find-the-least-common-multiple.cpp
@@ -7,7 +7,7 @@ int n, m;
 int gcd(int a, int b) {
     int g = 0;
 
-    for(int i = 2; i <= min(a, b); ++i) {
+    for(int i = 1; i <= min(a, b); ++i) {
         if(a % i == 0 && b % i == 0) {
             g = i;
         }
