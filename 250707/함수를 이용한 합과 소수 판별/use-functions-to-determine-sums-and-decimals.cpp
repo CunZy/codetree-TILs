c67944c5383a4cf4893c902692d117d19f901d76
CunZy <140526291+CunZy@users.noreply.github.com>
2025-07-07 14:59:55 +0900

    Title: 함수를 이용한 합과 소수 판별, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250707/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+int a, b;
+
+bool isTrue2(int i) {
+    int t = 0;
+    while(i > 0) {
+        t += (i % 10);
+        i /= 10;
+    }
+
+    return t % 2 == 0;
+}
+
+bool isTrue(int i) {
+    if(i == 1) {
+        return false;
+    }
+
+    for(int j = 2; j < i; ++j) {
+        if(i % j == 0) {
+            return false;
+        }
+    }
+
+    return isTrue2(i);
+}
+
+int main() {
+    cin >> a >> b;
+
+    // Please write your code here.
+    int c = 0;
+
+    for(int i = a; i <= b; ++i) {
+        if(isTrue(i)) {
+            c++;
+        }
+    }
+
+    cout << c;
+    return 0;
+}
