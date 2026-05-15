db3f9cd595e3879f5d837275934ca878703beb79
Cunzy <140526291+CunZy@users.noreply.github.com>
2026-05-15 11:32:55 +0900

    [Passed] G & H 반전시키기 2 (8ms, 0MB)

--- trail5/G & H 반전시키기 2/reversing-g-and-h-2.cpp
+++ trail5/G & H 반전시키기 2/reversing-g-and-h-2.cpp
@@ -1,32 +1,38 @@
 #include <iostream>
 using namespace std;
 
-int n;
-string a, b;
-int t[1000];
+int a[1000];
 
 int main() {
-    // Please write your code here.
-    cin >> n;
-
-    cin >> a >> b;
+    int n;
+    string q, w;
+    cin >> n >> q >> w;
 
     for(int i = 0; i < n; ++i) {
-        if(a[i] != b[i]) t[i] = 0;
-        else t[i] = 1;
+        if(q[i] != w[i]) a[i] = 0;
+        else a[i] = 1;
     }
 
     int cnt = 0;
+    
+    // for(int i = 0; i < n; ++i) {
+    //     cout << a[i] << " ";
+    // }
+    // cout << endl;
 
     for(int i = n - 1; i >= 0; --i) {
-        if(t[i] == 0) {
+        if(a[i] == 0) {
             ++cnt;
             for(int j = 0; j <= i; ++j) {
-                t[j] = 1 - t[j];
+                a[j] = 1 - a[j];
             }
         }
+
+        // for(int j = 0; j < n; ++j) {
+        //     cout << a[j] << " ";
+        // }
+        // cout << endl;
     }
 
     cout << cnt;
-    return 0;
 }
