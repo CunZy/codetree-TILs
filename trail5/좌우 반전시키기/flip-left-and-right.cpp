3a2a72ae1a8ce6a493d10e2625c9ca3d4c71429f
Cunzy <140526291+CunZy@users.noreply.github.com>
2026-05-15 11:23:45 +0900

    [Passed] 좌우 반전시키기 (8ms, 0MB)

--- trail5/좌우 반전시키기/flip-left-and-right.cpp
+++ trail5/좌우 반전시키기/flip-left-and-right.cpp
@@ -3,8 +3,28 @@ using namespace std;
 
 int a[100];
 
+int Count(int n) {
+    int cnt = 0;
+    for(int i = 0; i < n; ++i) {
+        if(i == n - 1 && a[i] == 0) return -1;
+        if(a[i] == 0) {
+            ++cnt;
+            if(i == n - 2) {
+                a[i] = 1 - a[i];
+                a[i + 1] = 1 - a[i + 1];
+            }
+            else {
+                a[i] = 1 - a[i];
+                a[i + 1] = 1 - a[i + 1];
+                a[i + 2] = 1 - a[i + 2];
+            }
+        } 
+    }
+
+    return cnt;
+}
+
 int main() {
-    // Please write your code here.
     int n;
     cin >> n;
 
@@ -12,23 +32,5 @@ int main() {
         cin >> a[i];
     }
 
-    int cnt = 0;
-    int answer = -1;
-
-    for(int i = 0; i < n - 1; ++i) {
-        if(a[i] == 0) {
-            ++cnt;
-            for(int j = i; j < i + 3; ++j) {
-                if(j >= n) break;
-
-                a[j] = 1 - a[j];
-            }
-        }
-    }
-
-    if(a[n - 1] == 1) answer = cnt;
-
-    cout << answer;
-
-    return 0;
+    cout << Count(n);
 }
