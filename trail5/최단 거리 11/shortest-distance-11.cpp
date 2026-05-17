5a6ec1f83abb7b1bf5965322a1163aa0dda4e9a7
Cunzy <140526291+CunZy@users.noreply.github.com>
2026-05-17 14:27:58 +0900

    [Passed] 최단 거리 11 (67ms, 2MB)

--- trail5/최단 거리 11/shortest-distance-11.cpp
+++ trail5/최단 거리 11/shortest-distance-11.cpp
@@ -3,25 +3,25 @@ using namespace std;
 
 #define MAX_N 1000
 
+int graph[MAX_N + 1][MAX_N + 1];
 int n, m;
 int a, b;
+bool visited[MAX_N + 1];
 int dist[MAX_N + 1];
-int graph[MAX_N + 1][MAX_N + 1];
-bool visit[MAX_N + 1];
 
 int main() {
-    // Please write your code here.
     cin >> n >> m;
 
     for(int i = 0; i < m; ++i) {
-        int x, y, edge;
-        cin >> x >> y >> edge;
+        int s, e, v;
+        cin >> s >> e >> v;
 
-        graph[x][y] = edge;
-        graph[y][x] = edge;
+        graph[s][e] = v;
+        graph[e][s] = v;
     }
 
     cin >> a >> b;
+
 
     for(int i = 1; i <= n; ++i) {
         dist[i] = (int)1e9;
@@ -30,29 +30,25 @@ int main() {
 
     for(int i = 1; i <= n; ++i) {
         int min_idx = -1;
+
         for(int j = 1; j <= n; ++j) {
-            if(visit[j]) continue;
-            if(min_idx == -1 || dist[min_idx] > dist[j]) {
-                min_idx = j;
-            }
+            if(visited[j]) continue;
+
+            if(min_idx == -1 || dist[min_idx] > dist[j]) min_idx = j;
         }
 
-        visit[min_idx] = true;
+        visited[min_idx] = true;
 
         for(int j = 1; j <= n; ++j) {
             if(graph[min_idx][j] == 0) continue;
 
-            int edge = graph[min_idx][j];
+            int t = graph[min_idx][j] + dist[min_idx];
 
-            int new_dist = dist[min_idx] + edge;
-
-            if(dist[j] > new_dist) {
-                dist[j] = new_dist;
+            if(t < dist[j]) {
+                dist[j] = t;
             }
         }
     }
-
-    // for(int i = 1; i <= n; ++i) cout << dist[i] << " ";
 
     cout << dist[a] << endl;
 
@@ -62,14 +58,12 @@ int main() {
     while(x != b) {
         for(int i = 1; i <= n; ++i) {
             if(graph[i][x] == 0) continue;
-            
+
             if(dist[i] + graph[i][x] == dist[x]) {
                 x = i;
-                // cout << x << " " << i << endl;
                 break;
             }
         }
         cout << x << " ";
     }
-    return 0;
 }
