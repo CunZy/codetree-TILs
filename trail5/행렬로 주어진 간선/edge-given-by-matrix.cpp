dba67ea0ee0a86b231b36a9d6b93eb22be20b90e
Cunzy <140526291+CunZy@users.noreply.github.com>
2026-05-18 12:31:05 +0900

    [Passed] 행렬로 주어진 간선 (8ms, 0MB)

--- trail5/행렬로 주어진 간선/edge-given-by-matrix.cpp
+++ trail5/행렬로 주어진 간선/edge-given-by-matrix.cpp
@@ -4,35 +4,30 @@ using namespace std;
 #define MAX_N 100
 
 int n;
-int d[MAX_N + 1][MAX_N + 1];
+int graph[MAX_N + 1][MAX_N + 1];
 
 int main() {
-    // Please wrie your code here.
     cin >> n;
-
     for(int i = 1; i <= n; ++i) {
         for(int j = 1; j <= n; ++j) {
-            cin >> d[i][j];
+            cin >> graph[i][j];
         }
     }
 
-    for(int i = 1; i <= n; ++i) {
-        d[i][i] = 1;
-    }
+    for(int i = i; i <= n; ++i) graph[i][i] = 1;
 
     for(int k = 1; k <= n; ++k) {
         for(int i = 1; i <= n; ++i) {
             for(int j = 1; j <= n; ++j) {
-                if(d[i][k] == 1 && d[k][j] == 1) d[i][j] = 1;
+                if(graph[i][k] && graph[k][j]) graph[i][j] = 1;
             }
         }
     }
 
     for(int i = 1; i <= n; ++i) {
         for(int j = 1; j <= n; ++j) {
-            cout << d[i][j] << " ";
+            cout << graph[i][j] << " ";
         }
-        cout << endl;
+        cout << endl;                    
     }
-    return 0;
 }
