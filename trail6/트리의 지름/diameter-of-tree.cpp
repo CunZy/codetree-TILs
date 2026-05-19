42590e172de4588f9adb3cba4d7a61e8c984099f
Cunzy <140526291+CunZy@users.noreply.github.com>
2026-05-19 13:10:44 +0900

    [Passed] 트리의 지름 (140ms, 7MB)

--- trail6/트리의 지름/diameter-of-tree.cpp
+++ trail6/트리의 지름/diameter-of-tree.cpp
@@ -1,62 +1,67 @@
 #include <iostream>
-#include <tuple>
 #include <vector>
-#include <climits>
+#include <tuple>
 using namespace std;
 
 #define MAX_N 100000
 
-vector<pair<int, int>> graph[MAX_N + 1];
 int n;
-bool visit[MAX_N + 1];
+vector<pair<int, int>> graph[MAX_N + 1];
+bool visited[MAX_N + 1];
 int dist[MAX_N + 1];
 
-void DFS(int v) {
-    for(int i = 0; i < graph[v].size(); ++i) {
-        int cur, edge;
-        tie(cur, edge) = graph[v][i];
+void DFS(int idx) {
+    for(int i = 0; i < graph[idx].size(); ++i) {
+        int n_idx, d;
+        tie(n_idx, d) = graph[idx][i];
 
-        if(visit[cur]) continue;
-
-        visit[cur] = true;
-        dist[cur] = dist[v] + edge;
-        DFS(cur);
+        if(!visited[n_idx]) {
+            visited[n_idx] = true;
+            dist[n_idx] = dist[idx] + d;
+            DFS(n_idx);
+        }
     }
 }
 
 int main() {
-    // Please write your code here.
     cin >> n;
-    for(int i = 0; i < n - 1; ++i) {
-        int x, y, z;
-        cin >> x >> y >> z;
 
-        graph[x].push_back(make_pair(y, z));
-        graph[y].push_back(make_pair(x, z));
+    for(int i = 0; i < n; ++i) {
+        int s, e, v;
+        cin >> s >> e >> v;
+
+        graph[s].push_back(make_pair(e, v));
+        graph[e].push_back(make_pair(s, v));
     }
 
-    visit[1] = true;
+    for(int i = 1; i <= n; ++i) {
+        dist[i] = (int)1e9;
+    }
+
+    visited[1] = true;
     dist[1] = 0;
     DFS(1);
 
-    int max_idx = -1;
+    int m_idx = 1;
+
     for(int i = 1; i <= n; ++i) {
-        if(max_idx == -1 || dist[max_idx] < dist[i]) max_idx = i;
+        if(dist[m_idx] < dist[i]) m_idx = i;
     }
 
     for(int i = 1; i <= n; ++i) {
-        visit[i] = false;
-        dist[i] = 0;
+        dist[i] = (int) 1e9;
+        visited[i] = false;
     }
 
-    visit[max_idx] = true;
-    dist[max_idx] = 0;
-    DFS(max_idx);
+    visited[m_idx] = true;
+    dist[m_idx] = 0;
+    DFS(m_idx);
 
-    int answer = INT_MIN;
+    int answer = -1;
+
     for(int i = 1; i <= n; ++i) {
         answer = max(answer, dist[i]);
     }
+
     cout << answer;
-    return 0;
 }
