a727399ba49b3dded41a1b26bbe4dba72291b91b
Cunzy <140526291+CunZy@users.noreply.github.com>
2026-05-20 12:42:26 +0900

    [Passed] 노드의 정수 계산 (119ms, 12MB)

--- trail6/노드의 정수 계산/calculating-an-integer-for-a-node.cpp
+++ trail6/노드의 정수 계산/calculating-an-integer-for-a-node.cpp
@@ -1,56 +1,50 @@
 #include <iostream>
-#include <tuple>
 #include <vector>
 using namespace std;
 
 #define MAX_N 100000
 
+vector<int> graph[MAX_N + 1];
 int n;
-vector<pair<int, int>> a[MAX_N + 1];
+bool visited[MAX_N + 1];
 int dp[MAX_N + 1];
-bool visit[MAX_N + 1];
-int arr[MAX_N + 1];
+int value[MAX_N + 1];
 
 void DFS(int idx) {
-    for(int i = 0; i < a[idx].size(); ++i) {
-        int cur, edge;
-        tie(cur, edge) = a[idx][i];
+    for(int i = 0; i < graph[idx].size(); ++i) {
+        int t = graph[idx][i];
 
-        if(visit[cur]) continue;
-
-        visit[cur] = true;
-        DFS(cur);
+        if(!visited[t]) {
+            visited[t] = true;
+            DFS(t);
+        }
     }
-
-    dp[idx] = arr[idx];
-    for(int i = 0; i < a[idx].size(); ++i) {
-        int cur, edge;
-        tie(cur, edge) = a[idx][i];
-        if(dp[cur] >= 0) dp[idx] += dp[cur];
+    
+    int sum_child = 0;
+    for(int i = 0; i < graph[idx].size(); ++i) {
+        if(value[graph[idx][i]] > 0) sum_child += value[graph[idx][i]];
     }
+    value[idx] += sum_child;
 }
 
 int main() {
-    // Please write your code here.
     cin >> n;
-    for(int i = 2; i <= n; ++i) {
-        int t, q, p;
-        cin >> t >> q >> p;
 
-        if(t) {
-            a[p].push_back(make_pair(i, q));
-            arr[i] = q;
+    for(int i = 2; i <= n; ++i) {
+        int t, a, p;
+        cin >> t >> a >> p;
+
+        if(t == 1) {
+            value[i] = a;
+            graph[p].push_back(i);
         }
         else {
-            a[p].push_back(make_pair(i, -q));
-            arr[i] = -q;
+            value[i] = -a;
+            graph[p].push_back(i);
         }
     }
 
-    visit[1] = true;
+    visited[1] = true;
     DFS(1);
-
-    cout << dp[1];
-
-    return 0;
+    cout << value[1];
 }
