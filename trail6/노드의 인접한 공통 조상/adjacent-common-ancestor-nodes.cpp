f1b31707e4a2789fc275c18d27cdff7a995b92c3
Cunzy <140526291+CunZy@users.noreply.github.com>
2026-05-21 12:34:27 +0900

    [Passed] 노드의 인접한 공통 조상 (9ms, 1MB)

--- trail6/노드의 인접한 공통 조상/adjacent-common-ancestor-nodes.cpp
+++ trail6/노드의 인접한 공통 조상/adjacent-common-ancestor-nodes.cpp
@@ -5,62 +5,55 @@ using namespace std;
 #define MAX_N 10000
 
 int n;
+vector<int> tree[MAX_N + 1];
 int depth[MAX_N + 1];
 int parent[MAX_N + 1];
-vector<int> a[MAX_N + 1];
-bool visit[MAX_N + 1];
 
 void DFS(int idx) {
-    for(int i = 0; i < a[idx].size(); ++i) {
-        int cur = a[idx][i];
-        if(visit[cur]) continue;
+    for(int i = 0; i < tree[idx].size(); ++i) {
+        int t = tree[idx][i];
 
-        // parent[cur] = idx;
-        depth[cur] = depth[idx] + 1;
-        visit[cur] = true;
-        DFS(cur);
+        depth[t] = depth[idx] + 1;
+        // cout << idx << " " << t << " " << depth[t] << " " << depth[idx] << endl;
+        DFS(t);
     }
 }
 
 int main() {
-    // Please write your code here.
     cin >> n;
-
     for(int i = 0; i < n - 1; ++i) {
-        int q, w;
-        cin >> q >> w;
-
-        a[q].push_back(w);
-        parent[w] = q;
-    }
-    
-    int id = 0;
-
-    for(int i = 1; i <= n; ++i) {
-        if(parent[i] == 0) {
-            id = i;
-            break;
-        }
+        int s, e;
+        cin >> s >> e;
+        parent[e] = s;
+        tree[s].push_back(e);
+        // cout << s << " " << e << endl;
     }
 
-    visit[id] = true;
-    depth[id] = 0;
-    DFS(id);
+    int a, b;
+    cin >> a >> b;
+    // cout << a << " " << b << endl;
 
-    int q, w;
-    cin >> q >> w;
+    int q = n;
+    while(parent[q] != 0) q = parent[q];
+    // cout << q << endl;
 
-    while(depth[q] != depth[w]) {
-        if(depth[q] > depth[w]) q = parent[q];
-        else w = parent[w];
+    DFS(q);
+    // cout << depth[a] << " " << depth[b] << endl;
+    // cout << endl;
+    // cout << a << " " << b << " " << depth[a] << " " << depth[b] << endl;
+
+    while(depth[a] != depth[b]) {
+        if(depth[a] > depth[b]) a = parent[a];
+        else b = parent[b];
+        // cout << a << " " << b << " " << depth[a] << " " << depth[b] << endl;
     }
 
-    while(w != q) {
-        w = parent[w];
-        q = parent[q];
+    // cout << endl;
+    while(a != b) {
+        a = parent[a];
+        b = parent[b];
+        // cout << a << " " << b << endl;
     }
 
-
-    cout << q;
-    return 0;
+    cout << a;
 }
