31c953e35af11316dcb93e182849d6095a83c61b
Cunzy <140526291+CunZy@users.noreply.github.com>
2026-05-23 18:18:02 +0900

    [Passed] 노드의 공통 조상 2 (19ms, 3MB)

--- trail6/노드의 공통 조상 2/common-ancestor-of-node-2.cpp
+++ trail6/노드의 공통 조상 2/common-ancestor-of-node-2.cpp
@@ -5,38 +5,53 @@ using namespace std;
 #define MAX_N 50000
 #define MAX_H 16
 
-int n, q;
 vector<int> graph[MAX_N + 1];
+int parent[MAX_H][MAX_N + 1];
 bool visited[MAX_N + 1];
-int parent[MAX_H + 1][MAX_N + 1];
 int depth[MAX_N + 1];
+int n;
 
 void DFS(int idx) {
     for(int i = 0; i < graph[idx].size(); ++i) {
         int t = graph[idx][i];
-
         if(!visited[t]) {
+            visited[t] = true;
             parent[0][t] = idx;
             depth[t] = depth[idx] + 1;
-            visited[t] = true;
             DFS(t);
         }
     }
 }
 
+void swap(int &a, int &b) {
+    int t = a;
+    a = b;
+    b = t;
+}
+
+int LCA(int a, int b) {
+    if(a == b) return a;
+    for(int h = MAX_H; h >= 0; --h) {
+        if(parent[h][a] != parent[h][b]) {
+            a = parent[h][a];
+            b = parent[h][b];
+        }
+    }
+
+    return parent[0][a];
+}
+
 int main() {
-    // Please write your code here.
     cin >> n;
-    for(int i = 1; i <= n - 1; ++i) {
+
+    for(int i = 0; i < n - 1; ++i) {
         int s, e;
         cin >> s >> e;
-
         graph[s].push_back(e);
         graph[e].push_back(s);
     }
 
     visited[1] = true;
-    depth[1] = 0;
     DFS(1);
 
     for(int i = 1; i <= MAX_H; ++i) {
@@ -45,48 +60,19 @@ int main() {
         }
     }
 
-    // for(int i = 0; i <= md; ++i) {
-    //     for(int j = 1; j <= n; ++j) {
-    //         cout << parent[i][j] << " ";
-    //     }
-    //     cout << endl;
-    // }
-
+    int q;
     cin >> q;
 
     for(int i = 0; i < q; ++i) {
         int a, b;
         cin >> a >> b;
 
-        for(int j = MAX_H; j >= 0; --j) {
-            int t = abs(depth[a] - depth[b]);
-            if(t == 0) break;
-            if(t >= (1 << j)) {
-                if(depth[a] > depth[b]) {
-                    a = parent[j][a];
-                }
-                else {
-                    b = parent[j][b];
-                }
-            }
-        }
-        // cout << endl;
-        // cout << a << " " << b << endl;
-        bool isTrue = false;
-        if(a == b) {
-            cout << a << endl;
-            isTrue = true;
+        if(depth[a] < depth[b]) swap(a, b);
+
+        for(int h = MAX_H; h >= 0; --h) {
+            if(depth[a] - depth[b] >= (1 << h)) a = parent[h][a];
         }
 
-        for(int j = MAX_H; j >= 0; --j) {
-            if(parent[j][a] != parent[j][b]) {
-                a = parent[j][a];
-                b = parent[j][b];
-            }
-        }
-        // cout << a << " " << b << endl;
-        if(!isTrue) cout << parent[0][a] << endl;
+        cout << LCA(a, b) << endl;
     }
-
-    return 0;
 }
