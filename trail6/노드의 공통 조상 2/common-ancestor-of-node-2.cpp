a848179c9f3a61dc1cc1de558cf92f6ca0102524
Cunzy <140526291+CunZy@users.noreply.github.com>
2026-05-25 12:41:57 +0900

    [Passed] 노드의 공통 조상 2 (21ms, 3MB)

--- trail6/노드의 공통 조상 2/common-ancestor-of-node-2.cpp
+++ trail6/노드의 공통 조상 2/common-ancestor-of-node-2.cpp
@@ -5,32 +5,34 @@ using namespace std;
 #define MAX_N 50000
 #define MAX_H 16
 
+int n;
 vector<int> graph[MAX_N + 1];
-int parent[MAX_H][MAX_N + 1];
 bool visited[MAX_N + 1];
 int depth[MAX_N + 1];
-int n;
+int parent[MAX_H + 1][MAX_N + 1];
 
 void DFS(int idx) {
     for(int i = 0; i < graph[idx].size(); ++i) {
         int t = graph[idx][i];
+
         if(!visited[t]) {
             visited[t] = true;
-            parent[0][t] = idx;
             depth[t] = depth[idx] + 1;
+            parent[0][t] = idx;
             DFS(t);
         }
     }
 }
 
-void swap(int &a, int &b) {
-    int t = a;
-    a = b;
-    b = t;
-}
-
 int LCA(int a, int b) {
+    if(depth[a] < depth[b]) return LCA(b, a);
+
+    for(int h = MAX_H; h >= 0; --h) {
+        if(depth[a] - depth[b] >= 1 << h) a = parent[h][a];
+    }
+
     if(a == b) return a;
+
     for(int h = MAX_H; h >= 0; --h) {
         if(parent[h][a] != parent[h][b]) {
             a = parent[h][a];
@@ -47,31 +49,27 @@ int main() {
     for(int i = 0; i < n - 1; ++i) {
         int s, e;
         cin >> s >> e;
+
         graph[s].push_back(e);
         graph[e].push_back(s);
-    }
-
-    visited[1] = true;
-    DFS(1);
-
-    for(int i = 1; i <= MAX_H; ++i) {
-        for(int j = 1; j <= n; ++j) {
-            parent[i][j] = parent[i - 1][parent[i - 1][j]];
-        }
     }
 
     int q;
     cin >> q;
 
+    visited[1] = true;
+    depth[1] = 1;
+    DFS(1);
+
+    for(int h = 1; h <= MAX_H; ++h) {
+        for(int i = 1; i <= n; ++i) {
+            parent[h][i] = parent[h - 1][parent[h - 1][i]];
+        }
+    }
+
     for(int i = 0; i < q; ++i) {
         int a, b;
         cin >> a >> b;
-
-        if(depth[a] < depth[b]) swap(a, b);
-
-        for(int h = MAX_H; h >= 0; --h) {
-            if(depth[a] - depth[b] >= (1 << h)) a = parent[h][a];
-        }
 
         cout << LCA(a, b) << endl;
     }
