e0dac3f8fcec63c74c605f86bc95e471b8d0c2fd
Cunzy <140526291+CunZy@users.noreply.github.com>
2026-05-25 13:05:09 +0900

    [Passed] 친구의 키 (58ms, 4MB)

--- trail6/친구의 키/height-of-friends.cpp
+++ trail6/친구의 키/height-of-friends.cpp
@@ -1,47 +1,46 @@
 #include <iostream>
-#include <vector>
 #include <stack>
+#include <vector>
 using namespace std;
 
 #define MAX_N 100000
 
 int n, m;
-vector<int> t[MAX_N + 1];
-bool visit[MAX_N + 1];
+vector<int> graph[MAX_N + 1];
 stack<int> order;
+bool visited[MAX_N + 1];
 
 void DFS(int idx) {
-    for(int i = 0; i < t[idx].size(); ++i) {
-        int cur = t[idx][i];
-        if(visit[cur]) continue;
-
-        visit[cur] = true;
-        DFS(cur);
+    for(int i = 0; i < graph[idx].size(); ++i) {
+        int t = graph[idx][i];
+        if(!visited[t]) {
+            visited[t] = true;
+            DFS(t);
+        }
     }
 
     order.push(idx);
 }
 
 int main() {
-    // Please write your code here.
     cin >> n >> m;
 
     for(int i = 0; i < m; ++i) {
         int a, b;
         cin >> a >> b;
-        t[a].push_back(b);
+
+        graph[a].push_back(b);
     }
 
     for(int i = 1; i <= n; ++i) {
-        if(visit[i]) continue;
-
-        visit[i] = true;
-        DFS(i);
+        if(!visited[i]) {
+            visited[i] = true;
+            DFS(i);
+        }
     }
 
     while(!order.empty()) {
         cout << order.top() << " ";
         order.pop();
     }
-    return 0;
 }
