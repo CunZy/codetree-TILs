68c0154d0850801fa626b077cb0ec0928f9ccfa3
Cunzy <140526291+CunZy@users.noreply.github.com>
2026-05-26 13:47:26 +0900

    [Passed] 친구의 키 2 (53ms, 3MB)

--- trail6/친구의 키 2/height-of-friends-2.cpp
+++ trail6/친구의 키 2/height-of-friends-2.cpp
@@ -1,71 +1,45 @@
 #include <iostream>
-#include <vector>
 #include <queue>
+#include <vector>
+using namespace std;
 
 #define MAX_N 100000
 
-using namespace std;
-
-// 변수 선언
 int n, m;
-
-vector<int> edges[MAX_N + 1];
-
-// 진입차수를 관리합니다.
-int indegree[MAX_N + 1];
-
-// 위상정렬을 위한 큐를 관리합니다.
+vector<int> graph[MAX_N + 1];
+int degree[MAX_N + 1];
 queue<int> q;
 
 int main() {
-    // 입력:
     cin >> n >> m;
-
-    // 인접리스트로 관리합니다.
-    for(int i = 1; i <= m; i++) {
-        int x, y;
-        cin >> x >> y;
-
-        edges[x].push_back(y); 
-        indegree[y]++; // 진입차수를 갱신합니다.  
+    for(int i = 0; i < m; ++i) {
+        int a, b;
+        cin >> a >> b;
+        graph[a].push_back(b);
+        degree[b]++;
     }
 
-    // 처음 indegree 값이 0인 곳이 시작점이 됩니다.
-    // 이 노드들을 queue에 넣어줍니다.
-    for(int i = 1; i <= n; i++)
-        if(!indegree[i])
-            q.push(i);
-    
-    // 위상정렬을 진행합니다.
-    // queue에 원소가 남아있다면 계속 진행합니다.
+    for(int i = 1; i <= n; ++i) {
+        if(degree[i] == 0) q.push(i);
+    }
     int cnt = 0;
     while(!q.empty()) {
-        // 가장 앞에 있는 원소를 뽑아줍니다.
-        int x = q.front();
+        int idx = q.front();
         q.pop();
 
-        // 큐에서 뽑힌 원소의 수를 세줍니다.
-        cnt++;
+        ++cnt;
 
-        // x에서 갈 수 있는 모든 곳을 탐색합니다.
-        for(int i = 0; i < (int) edges[x].size(); i++) {
-            int y = edges[x][i];
+        for(int i = 0; i < graph[idx].size(); ++i) {
+            int t = graph[idx][i];
 
-            // 해당 노드의 indegree를 1만큼 감소시켜줍니다.
-            indegree[y]--;
+            degree[t]--;
 
-            // 비로소 indegree가 0이 되었다면
-            // queue에 새로 넣어줍니다.
-            if(!indegree[y])
-                q.push(y);
+            if(!degree[t]) {
+                q.push(t);
+            }
         }
     }
 
-    // 큐에 총 n개의 수가 들어갔다면 사이클 없이 정상적으로 topological sort가 구해졌다는 뜻이므로
-    // 모순이 없게 됩니다.
-    if(cnt == n)
-        cout << "Consistent";
-    else
-        cout << "Inconsistent";
-    return 0;
+    if(cnt == n) cout << "Consistent";
+    else cout << "Inconsistent";
 }
