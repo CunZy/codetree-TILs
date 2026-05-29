11ed2c96a9d3f2932eb693c9a90feeb131ef001b
Cunzy <140526291+CunZy@users.noreply.github.com>
2026-05-29 12:44:33 +0900

    [Passed] 가장 긴 좌우 대칭인 문자열 찾기 2 (7ms, 2MB)

--- trail6/가장 긴 좌우 대칭인 문자열 찾기 2/longest-palindrome-2.cpp
+++ trail6/가장 긴 좌우 대칭인 문자열 찾기 2/longest-palindrome-2.cpp
@@ -4,44 +4,40 @@ using namespace std;
 #define MAX_N 200000
 
 int A[MAX_N + 1];
+string q;
 string t;
-int ans;
+int n;
 
 int main() {
-    string a;
-    cin >> a;
-    int n = a.length();
+    cin >> q;
 
-    
-    for(int i = 0; i < n; ++i) {
+    for(int i = 0; i < q.length(); ++i) {
         t += "#";
-        t += a[i];
+        t += q[i];
     }
     t += "#";
 
     n = t.length();
-
-    int r = -1, p = -1;
+    int p = -1, r = -1;
 
     for(int i = 0; i < n; ++i) {
         if(r < i) A[i] = 0;
         else {
             int ii = 2 * p - i;
-            A[i] = min((r - i), A[ii]);
+            A[i] = min(r - i, A[ii]);
         }
 
         while(i - A[i] - 1 >= 0 && i + A[i] + 1 < n && t[i - A[i] - 1] == t[i + A[i] + 1]) A[i]++;
 
-        if(i + A[i] > r) {
+        if(r < i + A[i]) {
             r = i + A[i];
             p = i;
         }
     }
 
+    int ans = 0;
     for(int i = 0; i < n; ++i) {
         ans = max(ans, 2 * A[i] + 1);
     }
-
     cout << ans / 2;
-
 }
