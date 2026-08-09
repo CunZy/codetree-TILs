a4e874a34d9a94711dfb284342fb5784ca4eb6b1
Cunzy <140526291+CunZy@users.noreply.github.com>
2026-08-09 12:37:35 +0900

    [Passed] 선택 정렬 구현 (2ms, 0MB)

--- trail3/선택 정렬 구현/implement-selection-sort.cpp
+++ trail3/선택 정렬 구현/implement-selection-sort.cpp
@@ -1,23 +1,31 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
 using namespace std;
+
+int a[100];
 
 int main() {
     // Please write your code here.
-    vector<int> a;
     int n;
     cin >> n;
 
     for(int i = 0; i < n; ++i) {
-        int t;
-        cin >> t;
-        a.push_back(t);
+        cin >> a[i];
     }
-    sort(a.begin(), a.end());
+
+    for(int i = 0; i < n - 1; ++i) {
+        int idx = i;
+        for(int j = i; j < n; ++j) {
+            if(a[idx] > a[j]) idx = j;
+        }
+
+        int t = a[idx];
+        a[idx] = a[i];
+        a[i] = t;
+    }
 
     for(int i = 0; i < n; ++i) {
         cout << a[i] << " ";
     }
+    
     return 0;
 }
