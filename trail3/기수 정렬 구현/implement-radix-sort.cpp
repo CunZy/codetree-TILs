9f082c68b4277c36b9f717912635ef1d829e619c
Cunzy <140526291+CunZy@users.noreply.github.com>
2026-08-11 22:05:09 +0900

    [Passed] 기수 정렬 구현 (30ms, 1MB)

--- trail3/기수 정렬 구현/implement-radix-sort.cpp
+++ trail3/기수 정렬 구현/implement-radix-sort.cpp
@@ -1,22 +1,44 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
+
+#define MAX_N 100000
+#define MAX_K 6
+#define MAX_DIGIT 10
+
 using namespace std;
 
-vector<int> a;
+// 변수 선언
+int n;
+int arr[MAX_N];
+
+void RadixSort() {
+    int p = 1;
+    for(int pos = 0; pos < MAX_K; pos++) {
+        vector<int> arr_new[MAX_DIGIT];
+        for(int i = 0; i < n; i++) {
+            int digit = (arr[i] / p) % 10;
+            arr_new[digit].push_back(arr[i]);
+        }
+
+        int index = 0;
+        for(int i = 0; i < MAX_DIGIT; i++)
+            for(int j = 0; j < (int) arr_new[i].size(); j++)
+                arr[index++] = arr_new[i][j];
+        
+        p *= 10;
+    }
+}
 
 int main() {
-    // Please write your code here.
-    int n;
+    // 입력
     cin >> n;
-    for(int i = 0; i < n; ++i) {
-        int t;
-        cin >> t;
-        a.push_back(t);
-    }
-    sort(a.begin(), a.end());
-    for(int i = 0; i < n; ++i) {
-        cout << a[i] << " ";
-    }
+
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+    
+    RadixSort();
+
+    for(int i = 0; i < n; i++)
+        cout << arr[i] << " ";
     return 0;
 }
