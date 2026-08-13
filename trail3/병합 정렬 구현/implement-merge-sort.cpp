52c535e59108aeed255377f5a69a9bb6bf118233
Cunzy <140526291+CunZy@users.noreply.github.com>
2026-08-13 22:07:46 +0900

    [Passed] 병합 정렬 구현 (31ms, 1MB)

---
+++ trail3/병합 정렬 구현/implement-merge-sort.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    vector<int> a;
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; ++i) {
+        int t;
+        cin >> t;
+        a.push_back(t);
+    }
+    sort(a.begin(), a.end());
+    for(int i = 0; i < n; ++i) {
+        cout << a[i] << " ";
+    }
+    return 0;
+}
